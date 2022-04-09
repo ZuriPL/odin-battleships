8c42abb594ec6a21d5e7ebf8f57829b2ba2bfce3
zuriii <michal.bak222@gmail.com>
2022-04-09 13:27:05 +0200

    created the ship factory and created tests for it

--- code/script.js
+++ code/script.js
@@ -1,4 +1,26 @@
-function sum(a, b) {
-    return a + b;
+const shipFactory = (length, pos, rotation) => {
+    const isSunk = () => {
+        if (hits.includes(false)) return false
+        return true
+    }
+    const hit = (x) => {
+        hits[x] = true
+    }
+    const getHits = () => {
+        return hits
+    }
+
+    let hits = []
+    for (let i = 0; i < length; i++) hits.push(false)
+
+    return {
+        length,
+        pos,
+        rotation,
+        isSunk,
+        hit,
+        getHits
+    }
 }
-export {sum}
+
+export { shipFactory }
--- tests/script.test.js
+++ tests/script.test.js
@@ -1,5 +1,26 @@
-import { sum } from "../code/script";
+import { shipFactory } from "../code/script";
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(5, 2)).toBe(7);
+const testShip1 = shipFactory(2, 'A1', 'right')
+
+test('check ship length', () => {
+    expect(testShip1.length).toBe(2);
+});
+
+test('check ship position', () => {
+    expect(testShip1.pos).toBe('A1');
+});
+
+test('check ship rotation', () => {
+    expect(testShip1.rotation).toBe('right');
+});
+
+test('test a hit', () => {
+    testShip1.hit(0)
+    expect(testShip1.getHits()).toEqual([true, false]);
+});
+
+test('test sinking the ship', () => {
+    expect(testShip1.isSunk()).toBe(false);
+    testShip1.hit(1)
+    expect(testShip1.isSunk()).toBe(true);
 });
