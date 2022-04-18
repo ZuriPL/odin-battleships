c34ee8a040aa2aa3a634e084af27fad68bc45da5
zuriii <michal.bak222@gmail.com>
2022-04-18 18:35:21 +0200

    added Gameboard factory

--- code/script.js
+++ code/script.js
@@ -1,4 +1,4 @@
-const shipFactory = (length, pos, rotation) => {
+const shipFactory = (length, x, y, rotation) => {
     const isSunk = () => {
         if (hits.includes(false)) return false
         return true
@@ -15,7 +15,8 @@ const shipFactory = (length, pos, rotation) => {
 
     return {
         length,
-        pos,
+        x,
+        y,
         rotation,
         isSunk,
         hit,
@@ -23,4 +24,47 @@ const shipFactory = (length, pos, rotation) => {
     }
 }
 
-export { shipFactory }
+const Gameboard = (size) => {
+    let ships = []
+    let hits = {}
+
+    const addShip = (ship) => {
+        ships.push(ship)
+    }
+
+    const isShipHit = (x, y) => {
+        // takes coordinates and checks whether a ship has been hit on this board
+        let result = false
+        for (const ship of ships) {
+            if (ship.rotation == 'horizontal') {
+                if (y == ship.y) {
+                    if (x <= ship.x <= x + ship.length) result = ship
+                }
+            } else {
+                if (x == ship.x) {
+                    if (y <= ship.y <= y + ship.length) result = ship
+                }
+            }
+        }
+        return result
+    }
+
+    const recieveAttack = (x, y) => {
+        if (!!isShipHit(x, y) == false) {
+            hits[`${x}-${y}`] = true
+        } else {
+            let ship = isShipHit(x, y)
+            ship.hit(ship.rotation == 'horizontal' ? (x - ship.x) : (y - ship.y))
+        }
+    }
+
+    return {
+        recieveAttack,
+        addShip,
+        isShipHit,
+        ships,
+        hits
+    }
+}
+
+export { shipFactory, Gameboard }
--- tests/script.test.js
+++ tests/script.test.js
@@ -1,17 +1,18 @@
-import { shipFactory } from "../code/script";
+import { shipFactory, Gameboard } from "../code/script";
 
-const testShip1 = shipFactory(2, 'A1', 'right')
+const testShip1 = shipFactory(2, 1, 1, 'vertical')
 
 test('check ship length', () => {
     expect(testShip1.length).toBe(2);
 });
 
 test('check ship position', () => {
-    expect(testShip1.pos).toBe('A1');
+    expect(testShip1.x).toBe(1);
+    expect(testShip1.y).toBe(1);
 });
 
 test('check ship rotation', () => {
-    expect(testShip1.rotation).toBe('right');
+    expect(testShip1.rotation).toBe('vertical');
 });
 
 test('test a hit', () => {
@@ -23,4 +24,37 @@ test('test sinking the ship', () => {
     expect(testShip1.isSunk()).toBe(false);
     testShip1.hit(1)
     expect(testShip1.isSunk()).toBe(true);
+});
+
+const testBoard = Gameboard(9)
+testBoard.addShip(
+    shipFactory(4, 1, 1, 'horizontal')
+)
+testBoard.addShip(
+    shipFactory(4, 3, 2, 'vertical')
+)
+
+test('test checking for a ship when it exists (horizontal)', () => {
+    expect(!!testBoard.isShipHit(2, 1)).toBe(true)
+});
+
+test('test checking for a ship when it exists (vertical)', () => {
+    expect(!!testBoard.isShipHit(3, 6)).toBe(true)
+});
+
+test('test checking for a ship when it doesnt exist', () => {
+    expect(!!testBoard.isShipHit(2, 2)).toBe(false)
+});
+
+testBoard.recieveAttack(1, 1)
+testBoard.recieveAttack(2, 2)
+
+test('test attacking an empty cell', () => {
+    expect(testBoard.hits['2-2']).toBe(true)
+    expect(testBoard.hits['2-3']).toBe(undefined)
+});
+
+test('test attacking an occupied cell', () => {
+    expect(testBoard.ships[0].getHits()[0]).toBe(true)
+    expect(testBoard.ships[0].getHits()[1]).toBe(false)
 });
